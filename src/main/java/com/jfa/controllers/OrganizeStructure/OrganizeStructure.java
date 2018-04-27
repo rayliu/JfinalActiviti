95e6a29521847056ff987c5b98dfb46f983a8e7c
Manning <kevin.dou@eeda123.com>
2018-04-27 17:45:44 +0800

    组织架构文件补交

---
+++ src/main/java/com/jfa/controllers/OrganizeStructure/OrganizeStructure.java
@@ -0,0 +1,155 @@
+package com.jfa.controllers.OrganizeStructure;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.jfa.interceptor.SetAttrLoginUserInterceptor;
+import com.jfa.util.MD5Util;
+import com.jfinal.kit.PropKit;
+import com.jfinal.kit.StrKit;
+import com.shuyan.wxl.annotation.JAction;
+
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.subject.Subject;
+
+import com.google.gson.Gson;
+import com.jfinal.aop.Before;
+import com.jfinal.core.Controller;
+import com.jfinal.log.Log;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import com.jfinal.plugin.activerecord.tx.Tx;
+
+
+//shiro注解, 要求必须登录才能访问
+@RequiresAuthentication
+@Before(SetAttrLoginUserInterceptor.class)
+//JAction注解, 无需在config中再配置
+@JAction(value = "/organize_structure",viewPath = "/organize_structure")
+public class OrganizeStructure extends Controller {
+    private Log logger = Log.getLog(OrganizeStructure.class);
+    Subject currentUser = SecurityUtils.getSubject();
+
+    //返回leave list页面
+    public void index() {
+        redirect("/organize_structure/edit");
+    }
+
+    //返回edit页面
+    public void edit() {
+        Record login_user = getAttr("user");
+        //String id = getPara("id");
+        List<Record> group1_list = Db.find("select * from t_rbac_group where parent_id is null");
+        for(int i = 0;i<group1_list.size();i++){
+        	List<Record> group2_list = Db.find("select * from t_rbac_group where parent_id = ?",group1_list.get(i).get("id"));
+        	group1_list.get(i).set("group2_list", group2_list);
+            for(int j = 0;j<group2_list.size();j++){
+            	List<Record> group3_list = Db.find("select * from t_rbac_group where parent_id = ?",group2_list.get(j).get("id"));
+            	group2_list.get(j).set("group3_list", group3_list);
+            }
+        }
+        setAttr("group1_list", group1_list);
+        render("edit.html");
+    }
+
+    @Before(Tx.class)
+    public void updateName(){
+    	String group_id = getPara("group_id");
+    	String group_name = getPara("group_name");
+    	Record group = new Record();
+    	boolean result = false;
+    	if(StrKit.notBlank(group_id)){
+    		group = Db.findById("t_rbac_group", group_id);
+    		group.set("name", group_name);
+    		result = Db.update("t_rbac_group",group);
+    	}
+    	renderJson("{\"result\":"+result+"}");
+    }
+    
+    @Before(Tx.class)
+    public void createGroup(){
+    	String group_id = getPara("group_id");
+    	String group_name = getPara("group_name");
+    	Record group = new Record();
+    	boolean result = false;
+    	if(StrKit.notBlank(group_id)){
+    		group.set("name", group_name);
+    		group.set("parent_id", group_id);
+    		result = Db.save("t_rbac_group",group);
+    	}
+    	renderJson("{\"result\":"+result+"}");
+    }
+  
+    public void userInfo(){
+    	String sLimit = "";
+        String condition = " ";
+
+        if (getPara("limit") != null && getPara("page") != null) {
+            int limit = getParaToInt("limit");
+            int page=getParaToInt("page")-1;
+            sLimit = " limit " + limit*page + ", " + limit;
+        }
+        
+    	String id = getPara("id");
+    	String sql = "select tru.*,trg.name group_name from t_rbac_ref_group_user trrgu"
+    			+ " left join t_rbac_user tru on tru.id = trrgu.user_id"
+    			+ " left join t_rbac_group trg on trg.id = trrgu.group_id"
+    			+ " where trrgu.group_id = "+id;
+    	List<Record> orderList = Db.find(sql+condition+" order by id desc "+sLimit);
+    	
+    	String sqlTotal = "select count(1) total from (" + sql + ") B";
+        Record rec = Db.findFirst(sqlTotal);
+
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("code", 0);
+        map.put("count", rec.get("total"));
+        map.put("data", orderList);
+
+        renderJson(map);
+    }
+    
+    @Before(Tx.class)
+    public void addUser(){
+    	String josnStr = getPara("params");
+    	Gson gson = new Gson();
+    	Map<String,Object> dto = gson.fromJson(josnStr,HashMap.class);
+    	String group_id = (String)dto.get("group_id");
+    	String name = (String)dto.get("name");
+    	String password =  MD5Util.encode("SHA1",(String)dto.get("password"));
+    	String cn_name = (String)dto.get("cn_name");
+    	String mobile = (String)dto.get("mobile");
+    	
+    	Record user = new Record();
+    	user.set("name", name);
+    	user.set("password", password);
+    	user.set("cn_name", cn_name);
+    	user.set("mobile", mobile);
+    	boolean user_result = Db.save("t_rbac_user", user);
+    	boolean result = false;
+    	if(user_result){
+    		Record re = new Record();
+    		re.set("group_id", group_id);
+    		re.set("user_id", user.get("id"));
+    		result = Db.save("t_rbac_ref_group_user", re);
+    	}
+    	renderJson("{\"result\":"+result+"}");
+    }
+    
+    @Before(Tx.class)
+	public void deleteUser(){
+		String id = getPara("id");
+		
+		boolean user_result = false;
+		int result = Db.delete("delete from t_rbac_ref_group_user where user_id = ?", id);
+		if(result>0){
+			Record user = Db.findById("t_rbac_user", id);
+			user_result = Db.delete("t_rbac_user",user);
+		}
+		renderJson("{\"result\":"+user_result+"}");
+	}
+}
---
+++ src/main/webapp/js/layui/organize_structure/edit.js
@@ -0,0 +1,212 @@
+$(function(){
+	layui.use(['jquery','table'], function(){
+		var table = layui.table, $=layui.$;
+		$('#sidebar_leave').addClass('layui-nav-itemed');
+		table.render({
+			elem: '#eeda_table'
+			,url:'/organize_structure/userInfo?id='+$("#group_id").val()
+			,width: 800
+			,text :{
+				none:'暂无相关数据'
+			}
+			//,cellMinWidth: 80 //全局定义常规单元格的最小宽度，layui 2.2.1 新增
+			,cols: [[
+					{field:'cn_name', title: '姓名'},
+					{field:'name', title: '账号'},
+					{field:'group_name', title: '部门'},
+					{ title: '操作',width:120,
+						templet:function(full){
+							return '<input type="button" class="layui-btn layui-btn-xs" lay-event="edit" value="编辑"/>'
+							+'<input type="button" class="layui-btn layui-btn-danger layui-btn-xs" lay-event="delete" value="删除"/>';
+						}
+					}
+			]]
+		});
+	});
+	
+	//架构树单击事件
+	$(".click").click(function(){
+		var self = $(this);
+		var id = self.children("span").attr("id");
+		$(".click").css("background-color","");
+		self.css("background-color","#d9e3ec");
+		
+		if(id!=$("#group_id").val()){
+			//单击不同的部门，将单击的部门名称回填到右边
+			$(".con_head_title").text(self.children("span").text());
+			$("#group_id").val(id);
+			//单击不同的部门，异步重载table
+			var table = layui.table;
+			table.reload('eeda_table', { url: '/organize_structure/userInfo',where: {id:$("#group_id").val()}});
+		}else{
+			//单击部门，如果部门id与右边的id相同，就隐藏
+			if("block"==self.parent().children("ul").css("display")){
+				self.parent().children("ul").css("display","none");
+			}else{
+				self.parent().children("ul").css("display","block");
+			}
+		}
+	});
+		
+	//修改部门名称
+	$("#updateGroupName").click(function(){
+		var group_id = $("#group_id").val();
+		
+		layer.prompt({title: '请输入部门名称', formType:0,value:$(".con_head_title").text()}, function(value,index){
+			layer.close(index);
+		  
+			$.post("/organize_structure/updateName",{group_id:group_id,group_name:value},function(data){
+				if(data.result){
+					layer.msg('修改成功',{icon:1});
+					$("#"+$("#group_id").val()).text(value);
+					$(".con_head_title").text(value);
+				}else{
+					layer.msg('修改失败',{icon:2});
+				}
+			});
+		  
+		});
+	});
+		
+	//新建子部门
+	$("#createGroup").click(function(){
+		var group_id = $("#group_id").val();
+		
+		layer.prompt({title: '请输入新建的部门名称', formType:0}, function(value,index){
+			layer.close(index);
+			
+			$.post("/organize_structure/createGroup",{group_id:group_id,group_name:value},function(data){
+				if(data.result){
+					layer.msg('新建成功',{icon:1});
+					appendMethod(group_id,value);
+				}else{
+					layer.msg('新建失败',{icon:2});
+				}
+			});
+		  
+		});
+	});
+		
+	//新建子部门时追加hmtl显示,达到实时显示
+	var appendMethod = function(group_id,value){
+		var html = '<li>'
+				 + '	<div class="organization" title="'+value+'">'
+			  	 + '		<div class="click">'
+				 + '			<i class="layui-icon">&#xe623;</i><i class="layui-icon">&#xe7a0;</i><span id="'+group_id+'">'+value+'</span>'
+				 + '		</div>'
+				 + '		<ul class="" style="margin-left:7%;"></ul>'
+			     + '	</div>'
+				 + '</li>'
+		$("#"+$("#group_id").val()).parent().parent().children("ul").append(html);
+	}
+	
+	//单击添加成员弹窗
+	$("#createUser").click(function(){
+		addUserPage("添加成员","","","","");
+	});
+	
+	//table监听，table里的单击事件都在里面
+	layui.use('table', function(){
+		var table = layui.table;
+		//监听工具条
+		table.on('tool(test)', function(obj){
+			var data = obj.data;
+			if(obj.event === 'edit'){
+				addUserPage("编辑成员",data.cn_name,data.name,data.password,data.mobile);
+			} else if(obj.event === 'delete'){
+				layer.confirm('确定删除该成员吗？',{title:'提示'}, function(){
+					$.post("/organize_structure/deleteUser",{id:data.id},function(data){
+						if(data.result){
+							layer.msg('删除成功',{icon:1});
+							obj.del();
+						}else{
+							layer.msg('删除失败',{icon:2});
+						}
+					}).fail(function(){
+			  			layer.msg('后台出错',{icon:2});
+			 		});;
+				});
+			}
+		});
+	});
+
+	//添加和编辑成员弹窗的html
+	var addUserPage = function(title,cn_name,name,password,mobile){
+		var style = "type=\"text\"";
+		if("编辑成员"==title){
+			style = "type=\"password\" disabled style=\"background-color:#ccc;\"";
+		}
+		//页面层
+		layer.open({
+		  type: 1,
+		  area: ['600px', '400px'], //宽高
+		  title:title,
+		  content: '<form id="order_form" class="layui-form" style="width:500px;margin:20px 0 0 18%;">'
+			  	  +'	<div class="layui-form-item layui-col-md8">'
+				  +'		<label class="layui-form-label"><span style="color:red;font-weight: bold;">*</span>用户名</label>'
+				  +'		<div class="layui-input-block">'
+				  +'			<input type="text" lay-verify="required" name="name" placeholder="请输入用户名" value="'+name+'" class="layui-input" />'
+				  +'		</div>'
+				  +'	</div>'
+				  +'	<div class="layui-form-item layui-col-md8">'
+				  +'		<label class="layui-form-label"><span style="color:red;font-weight: bold;">*</span>密码</label>'
+				  +'		<div class="layui-input-block">'
+				  +'			<input '+style+' lay-verify="required" name="password" placeholder="请输入密码" value="'+password+'" class="layui-input" />'
+				  +'		</div>'
+				  +'	</div>'
+				  +'	<div class="layui-form-item layui-col-md8">'
+				  +'		<label class="layui-form-label">中文名称</label>'
+				  +'		<div class="layui-input-block">'
+				  +'			<input type="text" name="cn_name" placeholder="请输入中文名称" value="'+cn_name+'" class="layui-input" />'
+				  +'		</div>'
+				  +'	</div>'
+				  +'	<div class="layui-form-item layui-col-md8">'
+				  +'		<label class="layui-form-label">手机号码</label>'
+				  +'		<div class="layui-input-block">'
+				  +'			<input type="text" name="mobile" placeholder="请输入手机号码" value="'+mobile+'" class="layui-input" />'
+				  +'		</div>'
+				  +'	</div>'
+				  +'	<div class="layui-form-item layui-col-md8" style="margin:15px 0 0 26%;">'
+				  +'		<input type="button" lay-submit value="确认" class="layui-btn layui-btn-norma" /><input type="button" id="cancelBtn" value="取消" class="layui-btn layui-btn-primary" />'
+				  +'	</div>'
+				  +'</form>'
+		});
+	}
+	
+	layui.use(['jquery', 'form', 'laydate', 'layedit'], function(){
+		var form = layui.form, $=layui.$, laydate=layui.laydate,layedit = layui.layedit;
+		
+		$('#sidebar_leave').addClass('layui-nav-itemed');
+	
+		//创建一个编辑器
+		var editIndex = layedit.build('LAY_demo_editor');
+		
+		  //监听提交
+		form.on('submit()',function(data){
+			var order = {};
+			order["group_id"] = $("#group_id").val();
+			$("#order_form input").each(function(){
+				order[$(this).attr("name")] = $(this).val();
+			});
+			
+			$.post("/organize_structure/addUser",{params:JSON.stringify(order)},function(data){
+	  			if(data.result){
+		  			layer.msg('保存成功',{icon:1});
+		  			var table = layui.table;
+					table.reload('eeda_table', { url: '/organize_structure/userInfo',where: {id:$("#group_id").val()}});
+					setTimeout(function(){
+						layer.closeAll("page");
+					},1000);
+	  			}else{
+		  			layer.msg('保存失败',{icon:2});
+	  			}
+	 		}).fail(function(){
+	  			layer.msg('后台出错',{icon:2});
+	 		});
+	 	});
+	});
+
+	$("body").on("click","#cancelBtn",function(){
+		layer.closeAll();
+	});
+});
